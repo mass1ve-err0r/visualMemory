6f0170f8488254ffb7869b9d55f530aa39e18a41
mass1ve-err0r <me@saadat.dev>
2021-02-21 14:30:05 +0100

    add Launcher * main entrypoint

---
+++ src/main/java/de/saadatbaig/visualMemory/Launcher.java
@@ -0,0 +1,38 @@
+/*
+ * Project: visualMemory
+ * Author:  Saadat M. Baig
+ * Date:    21.02.2021
+ * License: BSD-3-Clause-Attribution
+ */
+package de.saadatbaig.visualMemory;
+
+import de.saadatbaig.visualMemory.Controllers.HomeViewController;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+
+public class Launcher extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(getClass().getClassLoader().getResource("FXMLs/Home.fxml"));
+        Pane root = loader.load();
+        ((HomeViewController)loader.getController()).setHostServices(getHostServices());
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("visualMemory");
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
+    /* End */
+}
